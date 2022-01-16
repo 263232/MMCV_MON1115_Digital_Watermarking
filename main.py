50455b9a6c1a18f2a6fed63bf90ef67d79d7adf0
263232 <263232@student.pwr.edu.pl>
2022-01-16 13:35:12 +0100

    Edited

--- main.py
+++ main.py
@@ -14,6 +14,7 @@
 #   print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import args as args
 import numpy as np
 import cv2 as cv
 
@@ -24,37 +25,47 @@ def scale(image, scale_width):
     return cv.resize(image, (scale_width, new_height))
 
 
-watermark = scale(cv.imread('images/logo.png', cv.IMREAD_UNCHANGED), 150)
-(watermark_height, watermark_width) = watermark.shape[:2]
-image = scale(cv.imread('images/road.jpg'), 900)
-(image_height, image_width) = image.shape[:2]
-image = cv.cvtColor(image, cv.COLOR_BGR2BGRA)
+def visible():
+    watermark = scale(cv.imread('images/logo.png', cv.IMREAD_UNCHANGED), 150)
+    #watermark_ = cv.imread(args["watermark"], cv.IMREAD_UNCHANGED)
+    #if args["correct"] > 0:
+     #   (B, G, R, A) = cv.split(watermark_)
+     #   B = cv.bitwise_and(B, B, mask=A)
+     #   G = cv.bitwise_and(G, G, mask=A)
+      #  R = cv.bitwise_and(R, R, mask=A)
+      #  watermark = cv.merge([B, G, R, A])
 
-overlay = np.zeros((image_height, image_width, 4), dtype='uint8')
-#overlay[0:watermark_height, 0:watermark_width] = watermark
-#overlay[0:watermark_height, image_width-watermark_width:image_width] = watermark
-overlay[image_height-watermark_height:image_height, image_width-watermark_width:image_width] = watermark
+    (watermark_height, watermark_width) = watermark.shape[:2]
+    image = scale(cv.imread('images/road.jpg'), 900)
+    (image_height, image_width) = image.shape[:2]
+    image = cv.cvtColor(image, cv.COLOR_BGR2BGRA)
 
+    overlay = np.zeros((image_height, image_width, 4), dtype='uint8')
+    # overlay[0:watermark_height, 0:watermark_width] = watermark
+    # overlay[0:watermark_height, image_width-watermark_width:image_width] = watermark
+    overlay[image_height - watermark_height:image_height, image_width - watermark_width:image_width] = watermark
 
-output1 = image.copy()
-output2 = image.copy()
-output3 = image.copy()
-cv.addWeighted(overlay, 1.0, image, 1.0, 0, image)
+    output1 = image.copy()
+    output2 = image.copy()
+    output3 = image.copy()
+    cv.addWeighted(overlay, 1.0, image, 1.0, 0, image)
 
-cv.addWeighted(overlay, 0.1, image, 1.0, 0, output1)
-cv.addWeighted(overlay, 0.5, image, 1.0, 0, output2)
-cv.addWeighted(overlay, 1.0, image, 1.0, 0, output3)
+    cv.addWeighted(overlay, 0.1, image, 1.0, 0, output1)
+    cv.addWeighted(overlay, 0.5, image, 1.0, 0, output2)
+    cv.addWeighted(overlay, 1.0, image, 1.0, 0, output3)
 
-cv.imwrite("WatermarkedImage.png", image)
+    cv.imwrite("WatermarkedImage.png", image)
 
-while True:
-    cv.imshow("Overlay", overlay)
-    cv.imshow("Image", image)
-    cv.imshow("Watermark", watermark)
+    while True:
+        cv.imshow("Overlay", overlay)
+        cv.imshow("Image", image)
+        cv.imshow("Watermark", watermark)
 
-    cv.imshow("Overlay1", output1)
-    cv.imshow("Image1", output2)
-    cv.imshow("Watermark1", output3)
+        cv.imshow("Overlay1", output1)
+        cv.imshow("Image1", output2)
+        cv.imshow("Watermark1", output3)
 
-    if cv.waitKey(1) == ord('q'):
-        break
+        if cv.waitKey(1) == ord('q'):
+            break
+if __name__ == '__main__':
+    visible()
